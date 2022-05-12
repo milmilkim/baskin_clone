3aca9f2bfbfcb56140bd624a8d5a65d099902d95
milmilkim <soundkim27@gmail.com>
2022-05-12 21:02:59 +0900

    슬라이드 구현중
    힘들다...

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import TopBanner from './TopBanner';
+import Swipe from './Swipe';
 
 function Content() {
   return (
     <div style={{ marginTop: '6px' }}>
       <TopBanner />
+      <Swipe />
     </div>
   );
 }
