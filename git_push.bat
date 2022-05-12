364ebaac4a3822974c5d37c23459dac547b1a7b6
smilemet <smilemet@gmail.com>
2022-05-12 23:08:58 +0900

    modified by 諛⑸???

---
+++ git_push.bat
@@ -0,0 +1,3 @@
+git add --all
+git commit -m "modified by 방미소"
+git push origin main
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,53 @@
-import React from 'react';
+import React, { useState } from "react";
+import styled from "styled-components";
+const FooterContainer = styled.footer``;
 
 function Footer() {
-  return <div>Footer</div>;
+  const [list] = useState([
+    {
+      name: "점포개설문의",
+      url: "",
+    },
+    {
+      name: "채용문의",
+      url: "",
+    },
+    {
+      name: "윤리신고센터",
+      url: "",
+    },
+    {
+      name: "이용약관",
+      url: "",
+    },
+    {
+      name: "개인정보처리방침",
+      url: "",
+    },
+    {
+      name: "영상정보처리기기운영관리방침",
+      url: "",
+    },
+    {
+      name: "거래희망회사 사전등록",
+      url: "",
+    },
+  ]);
+  return (
+    <FooterContainer>
+      <div className="inner">
+        <ul className="">
+          {list.map((v) => {
+            return (
+              <li>
+                <a href={v.url}>{v.name}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </FooterContainer>
+  );
 }
 
 export default Footer;
