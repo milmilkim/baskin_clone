d46aff21ad816ea5126b1605691b423fedf6b8da
milmilkim <soundkim27@gmail.com>
2022-05-15 16:29:01 +0900

    add BrMenu

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,12 +3,14 @@ import TopBanner from './TopBanner';
 import Swipe from './Swipe';
 import Store from './Store';
 import Instagram from './Instagram';
+import BrMenu from './BrMenu';
 
 function Content() {
   return (
     <div style={{ marginTop: '6px' }}>
       <TopBanner />
       <Swipe />
+      <BrMenu />
       <Store />
       <Instagram />
     </div>
